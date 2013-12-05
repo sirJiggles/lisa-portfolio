b7d45e1b75f6c125510f2a5265abd300dac9439f
Gareth Fuller <gareth@honestideas.com>
2013-12-05 22:22:20 +0000

    First commit, just set things up

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// Define the appvars used in various functions etc first
+var appVars = {
+    tablet: $(window).width() <= 850 ? true : false,
+    mobile: $(window).width() <= 600 ? true : false,
+    desktop: $(window).width() >= 960 ? true : false,
+    largeScreen: $(window).width() >= 1530 ? true : false,
+    resizeTimer: null,
+    imagesSwapped: false
+}
+
+/*
+ * Main javascript file
+ * 
+ * require any libraries using juicer
+ * 
+ * @depends vendor/jquery-1.8.2.min.js
+ * @depends vendor/jquery.flexslider-min.js
+ * @depends app-functions.js
+ */
+
+$(window).ready(function () {
+    
+     // Generic resize function
+    $(window).resize(function(){
+        clearTimeout(appVars.resizeTimer);
+        appVars.resizeTimer = setTimeout(resizeWindowCallback, 500);
+    });
+    
+});
+
+$(window).load(function(){
+    // External link class JS
+    externalLinks();
+   
+});
---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+    <head>
+
+        <title>Lisa Jungen</title>
+
+        <!-- DNS PREFETCH -->
+        <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+
+        <!--[if lt IE 9]>
+            <script src="assets/js/vendor/htmlshiv.min.js"></script>
+        <![endif]-->
+
+        <script type="text/javascript">
+            var html = document.getElementsByTagName("html")[0];
+            if(html.className == "no-js") {
+                html.className = html.className.replace("no-js", "js");
+            } 
+            var siteAssetsPath = '/assets';
+        </script>
+
+        <!-- META -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1, minimum-scale=1" />
+        <meta name="description" content="Lisa Jungen - Personal acting portfolio" />
+        <meta name="author" content="Lisa Jungen" />
+        <meta name="keywords" content="actress, actress portfolio, cambridge actress, actress cambridge, lisa jungen, actress lisa jungen" />
+    
+        <!-- Humans file -->
+        <link rel="author" type="text/plain" href="/humans.txt" />
+
+        <!-- Favicons -->
+        <link rel="shortcut icon" href="assets/favicons/16.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="assets/favicons/57.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/favicons/72.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/favicons/114.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/favicons/144.png" />
+        
+        <!-- CSS -->
+        <link rel="stylesheet" href="assets/css/print.css" media="print" type="text/css"/>
+        
+              
+        <!--Google fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Limelight' rel='stylesheet' type='text/css'>
+
+
+    </head>
+    <!--[if IE 7 ]>    <body class="ie7"> <![endif]-->
+    <!--[if IE 8 ]>    <body class="ie8"> <![endif]-->
+    <!--[if IE 9 ]>    <body class="ie9"> <![endif]-->
+    <!--[if (gt IE 9)|!(IE)]><!--> <body> <!--<![endif]-->
+
+
+        <div id="site-wrapper">
+
+            <header role="banner">
+                
+            </header>
+
+            <!-- START OF THE GALLERY ROW -->
+            <div class="row">
+                <section class="flexslider">
+
+                </section>
+            </div>
+            <!-- END OF THE GALLERY ROW -->
+
+            <footer role="footer">
+
+            </footer>
+
+        </div>
+        <!-- end site wraper -->
+
+        <!-- Load all JS last -->
+        <script type="text/javascript" src="assets/js/script.min.js"></script>
+    
+    </body>
+</html>
